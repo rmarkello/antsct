21c34ea1fa56dc54d4dcefef3ef80e9b2ffe9686
Ross Markello <rossmarkello@gmail.com>
2018-02-14 13:25:22 -0500

    Singularity build must wait for docker push

--- make_singularity.sh
+++ make_singularity.sh
@@ -7,6 +7,7 @@ docker run -d -p 5000:5000 --restart=always --name registry registry:2
 # tag and push image to local registry
 docker tag antsct localhost:5000/antsct
 docker push localhost:5000/antsct
+wait ${!}
 # create singularity image from local registry; require singularity >=2.4.2
 SINGULARITY_NOHTTPS=true singularity pull docker://localhost:5000/antsct
 # remove registry and docker image
